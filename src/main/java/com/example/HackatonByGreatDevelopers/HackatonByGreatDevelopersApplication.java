c98fc46410bacc1523313c6c3a48ee45bcbc8a0d
NikitaShapovalovv <bigwolf41@mail.ru>
2022-12-02 22:32:14 +0600

    Чистый проект

---
+++ src/main/java/com/example/HackatonByGreatDevelopers/HackatonByGreatDevelopersApplication.java
@@ -0,0 +1,13 @@
+package com.example.HackatonByGreatDevelopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonByGreatDevelopersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonByGreatDevelopersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HackatonByGreatDevelopers/HackatonByGreatDevelopersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HackatonByGreatDevelopers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonByGreatDevelopersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
